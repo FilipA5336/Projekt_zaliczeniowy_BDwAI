fab49ab91265944bc858ab615e031a1c93130fdb
FilipA5336 <s168748@student.uken.krakow.pl>
2025-12-30 14:02:01 +0100

    Dodaj pliki projektów.

---
+++ Projekt_zaliczeniowy/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Projekt_zaliczeniowy.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
